035820075de256a60679a6a2f5ba6fa25bf779d4
Evan Busse <evanbusse@gmail.com>
2016-10-25 18:52:32 -0600

    Dynamically sized images

--- dynamics.js
+++ dynamics.js
@@ -1,14 +1,11 @@
-// 'use strict';
-//
-// var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=e0a89bb76a62729eced4ffa87ba8633c&lat=38&lon=-109&safe_search=1&per_page=20";
-// var src;
-//
-// $.getJSON(url + "&format=json&jsoncallback=?", function(data){
-//     $.each(data.photos.photo, function(i,item){
-//         src = "http://farm"+ item.farm +".static.flickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +"_m.jpg";
-//         $("<img/>").attr("src", src).appendTo("#images");
-//         if ( i === 10 ){
-//           return false;
-//         }
-//     });
-// });
+'use strict';
+
+$('#collapse').on('click', function(){
+  $('#map').toggle(500);
+});
+
+$('#images').on('click', function(event) {
+  if(event.target.id !== 'images'){
+    console.log(event.target);
+  }
+});
--- map.js
+++ map.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var map;
-var locations = [];
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -9,7 +8,7 @@ function initMap() {
             lat: 38.75408327579141,
             lng:  -105.380859375,
         },
-        zoom: 6
+        zoom: 4
     });
 
     map.addListener('click', function (event) {
@@ -20,36 +19,27 @@ function initMap() {
       var lat = coords.lat();
       var lng = coords.lng();
 
-      var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=e0a89bb76a62729eced4ffa87ba8633c&lat=" + lat + "&lon=" + lng + "&safe_search=1&per_page=20";
-      var src;
-
-      $.getJSON(url + "&format=json&jsoncallback=?", function(data){
-        $('#images').empty();
-          $.each(data.photos.photo, function(i,item){
-              src = "http://farm"+ item.farm +".static.flickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +"_m.jpg";
-              $("<img/>").attr("src", src).appendTo("#images");
-              if ( i === 10 ){
-                return false;
-              }
-          });
-      });
-
-      // window.addEventListener('keydown', function (event) {
-      //   if(event.keyCode === 13){
-      //     var name = event.target.value;
-      //
-      //     if(name !== ''){
-      //
-      //
-      //       event.target.value = "";
-      //       $('#textfield').hide(500);
-      //     }else{
-      //       Materialize.toast('Please enter a name for your pin', 4000, 'blue lighten-2');
-      //     }
-      //   }
-      // });
-
+      getImages(lat, lng);
 
     });
 
+}
+
+function getImages (lat, lng) {
+  var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=e0a89bb76a62729eced4ffa87ba8633c&lat=" + lat + "&lon=" + lng + "&safe_search=1&per_page=20";
+  var src;
+
+  $.getJSON(url + "&format=json&jsoncallback=?", function(data){
+      $('#images').empty();
+      $.each(data.photos.photo, function(i,item){
+          src = "http://farm"+ item.farm +".static.flickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +"_m.jpg";
+          $("<img/>").attr({
+            "src": src,
+            "class": "col s6 m4 l2",
+          }).appendTo("#images");
+          if ( i === 10 ){
+            return false;
+          }
+      });
+  });
 }
