6a64b9bd402a3f7b8a1b1be65f16c4d5f5ee56ab
Evan Busse <evanbusse@gmail.com>
2016-10-27 22:51:45 -0600

    Fixed repeating listeners

--- dynamics.js
+++ dynamics.js
@@ -1,40 +1,49 @@
 'use strict';
 
-// $(function(){
-//   $('#action_bar').slideToggle(800);
-// });
+var src;
 
-//expand images on click
 $('#images').on('click', function(event) {
 
-  if(event.target.id !== 'images'){
+    if (event.target.id !== 'images') {
 
-    $('#action_bar').slideDown(500); //Shows Action Bar
+        $('#action_bar').slideDown(500); //Shows Action Bar
 
-    $('#action_bar').click(function() { //Hides action bar after action
-      $(this).slideUp(500);
-    });
+        $('#action_bar').click(function() { //Hides action bar after action
+            $(this).slideUp(500);
+        });
 
-    //change image size
-    var src = event.target.src.replace('m.jpg', 'b.jpg');
+        //change image size
+        src = event.target.src.replace('m.jpg', 'b.jpg');
 
-    //expand icon funcitonality
-    $('#expand').on('click', function() {
-      $('#expanded').empty(); //empty expanded preview
+    }
+});
 
-      $("<img/>").attr({
-        "src": src,
-        "class": "big_preview",
-      }).appendTo('#expanded'); //add new image
+//expand icon funcitonality
+$('#expand').on('click', function() {
+  $('#expanded').empty(); //empty expanded preview
 
-      $('#expanded').fadeIn(500); //fade in expanded preview
-      Materialize.toast('Click again to close', 2000);
+  $("<img/>").attr({
+    "src": src,
+    "class": "big_preview",
+  }).appendTo('#expanded'); //add new image
 
-      $('#expanded').click(function(){ //handles closing expanded preview
-        $(this).fadeOut(500);
-      });
-    });
+  $('#expanded').fadeIn(500); //fade in expanded preview
+  Materialize.toast('Click again to close', 2000);
 
+  $('#expanded').click(function() { //handles closing expanded preview
+    $(this).fadeOut(500);
+  });
 
-  }
+});
+
+//Add to collection functionality
+$('#collect').on('click', function () {
+  Materialize.toast("Added to collection", 2000);
+});
+
+//Add tag functionality
+$('#tag').on('click', function() {
+  Materialize.toast("Add Tag", 2000);
+  //expand text box
+  //get input, seperate words, chips?
 });
