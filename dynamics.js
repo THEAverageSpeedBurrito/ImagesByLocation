be695ac0417cb239b6c6237dbda5a1ebcf8da6d8
Evan Busse <evanbusse@gmail.com>
2016-10-28 14:48:35 -0600

    Collection scroll works

--- dynamics.js
+++ dynamics.js
@@ -11,20 +11,24 @@ $('#images').on('click', function(event) {
 
         $('#action_bar').click(function() { //Hides action bar after action
             $(this).slideUp(500);
+            selected.removeAttribute('style');
         });
 
-        //change image size (medium to big)
-        src = event.target.src.replace('m.jpg', 'b.jpg');
+        src = event.target.src;
 
         //show active image
         selected = event.target;
-        selected.style.border = '5px solid red';
+        selected.style.border = '5px solid tomato';
     }
+    
 });
 
 //expand icon funcitonality
 $('#expand').on('click', function() {
 
+  //change image size (medium to big)
+  src = src.replace('m.jpg', 'b.jpg');
+  //remove red border
   selected.removeAttribute('style');
 
   $('#expanded').empty(); //empty expanded preview
@@ -44,14 +48,24 @@ $('#expand').on('click', function() {
 });
 
 //Add to collection functionality
-var collection = []; //collection storage
 
 $('#collect').on('click', function () {
 
   selected.removeAttribute('style');
   Materialize.toast("Added to collection", 2000);
 
-  collection.push(src);
+  $('#collection_outer').slideDown(500);
+
+  var newImage = $('<img>').attr({
+    'src': src,
+    'class': 'collected_img',
+  });
+
+  newImage.prependTo('#collection_inner');
+  var newImageWidth = newImage.width() + 20;
+
+  $('#collection_inner').width($('#collection_inner').width() + newImageWidth);
+  console.log($('#collection_inner').width());
 });
 
 
