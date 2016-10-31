5c9c5776bea8b12937c2ec2125f9408c8b41deaa
Evan Busse <evanbusse@gmail.com>
2016-10-31 11:38:19 -0600

    Created landing page

---
+++ Js/landingJS.js
@@ -0,0 +1,12 @@
+'use strict';
+
+$(function () {
+
+  $('#title_box').delay(800).animate({
+      opacity: 1,
+      width: '100%',
+      borderRadius: '0'
+  }, 1000);
+
+  $('h5, #getStarted').delay(1800).slideDown(800);
+});
